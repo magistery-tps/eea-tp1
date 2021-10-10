828f3ab55a9831a95c4b99bad656f80ab43726cb
adrianmarino <adrianmarino@gmail.com>
2021-10-10 12:46:44 -0300

    wip: pre-processing

--- src/preprocessing.R
+++ src/preprocessing.R
@@ -1,11 +1,8 @@
 library(pacman)
 p_load(dplyr)
 
-preprocess <- function(
-  df, 
-  excluded_variables = c('record')
-) {
-  load_train_set() %>% 
-    select(-all_of(excluded_variables))  %>%
+preprocess <- function(df, excluded_variables = c('record')) {
+  df %>% 
+    dplyr::select(-all_of(excluded_variables))  %>%
     mutate_if(is.character, factor)
 }
